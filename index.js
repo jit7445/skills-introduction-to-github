c525f0a33a6a446f2b3178be4ad4774099f197eb
jitender_verma <139689987+jit7445@users.noreply.github.com>
2026-04-10 17:04:21 +0530

    index file change

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let a=[1,2,3,4,4,5,6,7,8,9,10];
 let b=a.filter((item,index)=>{
     return a.indexOf(item)===index;
+    console.log(a.indexOf(item));
+    console.log(index);
 })
+
 console.log(b);
+
